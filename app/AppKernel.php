695c7342613f978bbb3966ef56895ad6f32f16ef
Laurent Meuwly <laurent@lmeuwly.ch>
2017-12-03 12:58:04 +0100

    update app dependencies

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -8,34 +8,73 @@ class AppKernel extends Kernel
     public function registerBundles()
     {
         $bundles = [
+        	
+        	new AppBundle\AppBundle(),
+        	
+        	// Symfony standard edition
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+        	new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
         	new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-        	        	
-        	new Sonata\CoreBundle\SonataCoreBundle(),
-        	new Sonata\BlockBundle\SonataBlockBundle(),
-            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
-            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
-        	new Sonata\EasyExtendsBundle\SonataEasyExtendsBundle(),
-            new Sonata\AdminBundle\SonataAdminBundle(),
+        	
+        	// Doctrine
+        	new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+                    	
+        	// KNP Helper
+        	new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+        	new Knp\Bundle\MarkdownBundle\KnpMarkdownBundle(),
+        	// new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+        	new Knp\Bundle\SnappyBundle\KnpSnappyBundle(),
+        	new Knp\DoctrineBehaviors\Bundle\DoctrineBehaviorsBundle(),
+        	
+        	// Sonata feature
         	new FOS\UserBundle\FOSUserBundle(),
         	new Sonata\UserBundle\SonataUserBundle('FOSUserBundle'),
-        	
-            new AppBundle\AppBundle(),
-        		
-        	new Knp\Bundle\MarkdownBundle\KnpMarkdownBundle(),
+        	new Application\Sonata\UserBundle\ApplicationSonataUserBundle(),
+        	new Application\Sonata\MediaBundle\ApplicationSonataMediaBundle(),
+        	// new Sonata\PageBundle\SonataPageBundle(),
+        	// new Sonata\NewsBundle\SonataNewsBundle(),
+        	new Sonata\MediaBundle\SonataMediaBundle(),
+        	new Sonata\TranslationBundle\SonataTranslationBundle(),
+        		     
+        	new CoopTilleuls\Bundle\CKEditorSonataMediaBundle\CoopTilleulsCKEditorSonataMediaBundle(),
         	new Ivory\CKEditorBundle\IvoryCKEditorBundle(),
+        	
+        	new Sonata\AdminBundle\SonataAdminBundle(),
+        	new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+        	
+        	
+            // Sonata foundation
+        	new Sonata\EasyExtendsBundle\SonataEasyExtendsBundle(),
+        	new Sonata\CoreBundle\SonataCoreBundle(),
+        	// new Sonata\IntlBundle\SonataIntlBundle(),
         	new Sonata\FormatterBundle\SonataFormatterBundle(),
+        	// new Sonata\CacheBundle\SonataCacheBundle(),
+        	new Sonata\BlockBundle\SonataBlockBundle(),
+        	// new Sonata\SeoBundle\SonataSeoBundle(),
+        	// new Sonata\ClassificationBundle\SonataClassificationBundle(),
+        	// new Sonata\NotificationBundle\SonataNotificationBundle(),
+        	// new Sonata\DatagridBundle\SonataDatagridBundle(),
+        		
+        		
+        	//new A2lix\AutoFormBundle\A2lixAutoFormBundle(),
+        	new A2lix\TranslationFormBundle\A2lixTranslationFormBundle(),
         		
         	new Pix\SortableBehaviorBundle\PixSortableBehaviorBundle(),
-        	new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
-        		
+        	new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),	
         	//new Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle(),
+        	new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
+        	
+        	new APY\DataGridBundle\APYDataGridBundle(),
+        	new Sg\DatatablesBundle\SgDatatablesBundle(),
+        	new Waldo\DatatableBundle\WaldoDatatableBundle(),
+        		
+        	new Xmon\ColorPickerTypeBundle\XmonColorPickerTypeBundle(),
+        		
+        	new Ivory\GoogleMapBundle\IvoryGoogleMapBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
@@ -43,6 +82,7 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
         }
 
         return $bundles;
