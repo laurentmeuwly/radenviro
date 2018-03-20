51611349dcedbe4114f11fa09864eada0eb5dbec
Laurent Meuwly <laurent@lmeuwly.ch>
2018-03-20 22:39:10 +0100

    remove constraint on isotope list for advanced search

--- src/AppBundle/Form/AdvancedSearchType.php
+++ src/AppBundle/Form/AdvancedSearchType.php
@@ -5,10 +5,13 @@ namespace AppBundle\Form;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Validator\Constraints\Callback;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 use AppBundle\Entity\Network;
+use AppBundle\Entity\Nuclide;
 use AppBundle\Entity\PredefinedNuclideList;
+
 
 
 class AdvancedSearchType extends AbstractType
@@ -22,43 +25,38 @@ class AdvancedSearchType extends AbstractType
 			->add('network', 'entity', array(
 					'mapped' => false,
 					'class' => 'AppBundle:Network',
-					//'property' => 'name',
 					'placeholder' => 'search.choose_network',
 					'label' => 'search.field.network',
 					'query_builder' => function (EntityRepository $er) {
-						return $er->createQueryBuilder('n')
-						->where('n.active=1');
+							return $er->createQueryBuilder('n')
+							->where('n.active=1');
 					},
-					//'data' => $session->get('network'),
 			))
 			->add('displayList', 'entity', array(
 					'mapped' => false,
+					'required' => false,
 					'class' => 'AppBundle:PredefinedNuclideList',
-					//'property' => 'name',
 					'placeholder' => 'search.choose_displaylist',
 					'label' => 'search.field.displaylist',
 					'query_builder' => function (EntityRepository $er) {
 					return $er->createQueryBuilder('l')
-					->where('l.active=1')
-					->orderBy('l.position', 'asc');
+							->where('l.active=1')
+							->orderBy('l.position', 'asc');
 					},
-					//'data' => $session->get('network'),
-					))
-					->add('nuclide', 'entity', array(
-							'mapped' => false,
-							'class' => 'AppBundle:Nuclide',
-							//'property' => 'name',
-							'placeholder' => 'search.choose_nuclide',
-							'label' => 'search.field.nuclide',
-							'query_builder' => function (EntityRepository $er) {
-							return $er->createQueryBuilder('n')
+			))
+			->add('nuclide', 'entity', array(
+					'mapped' => false,
+					'required' => false,
+					'class' => 'AppBundle:Nuclide',
+					'placeholder' => 'search.choose_nuclide',
+					'label' => 'search.field.nuclide',
+					'query_builder' => function (EntityRepository $er) {
+					return $er->createQueryBuilder('n')
 							->where('n.active=1');
-							},
-							
-							))
+					},				
+			))
 			->add('search', SubmitType::class, array('label' => 'label.search'))
 		;
 	}
-	
-	
+
 }
