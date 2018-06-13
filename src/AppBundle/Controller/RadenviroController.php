4d60f59d46ab112459effb8e4cf553ff7d18f638
Laurent Meuwly <laurent@lmeuwly.ch>
2018-06-13 11:02:24 +0200

    Add second map sample just for validation

--- src/AppBundle/Controller/RadenviroController.php
+++ src/AppBundle/Controller/RadenviroController.php
@@ -6,10 +6,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-use Omines\DataTablesBundle\Adapter\ArrayAdapter;
-use Omines\DataTablesBundle\Column\TextColumn;
-use Omines\DataTablesBundle\Column\DateTimeColumn;
-use Omines\DataTablesBundle\Controller\DataTablesTrait;
+
 
 
 class RadenviroController extends Controller
@@ -52,105 +49,6 @@ class RadenviroController extends Controller
 	}
 	
 	
-	/**
-	 * @Route("/map2", name="map2")
-	 */
-	public function mapAction(Request $request)
-	{
-		$session = $request->getSession();
-		 
-		$nuclide = $request->query->get('nuclide');
-		if($nuclide==null) $nuclide=54;
-		$session->set('nuclide', $nuclide);
-		 
-		$isAjax = $request->isXmlHttpRequest();
 	
-		$nuclide = $session->get('nuclide');
-		//$session->get('station');
-		 
-	
-		 
-		// Getting doctrine manager
-		$em = $this->getDoctrine()->getManager();
-	
-		// retrieve all active legends
-		$legends = $em->getRepository('AppBundle:Legend')->findBy(array('active' => 1), array('position' => 'ASC'));
-	
-		// retrieve all active site types
-		$siteTypes = $em->getRepository('AppBundle:SiteType')->findBy(array('active' => 1), array('position' => 'ASC'));
-	
-		// retrieve all active automatic networks
-		$automaticNetworks = $em->getRepository('AppBundle:AutomaticNetwork')->findBy(array('active' => 1), array('position' => 'ASC'));
-	
-		// retrieve all zoom areas
-		$zooms = $em->getRepository('AppBundle:MapZoom')->findAll();
-	
-		// build the table for last measure per station
-		$rawsql = $this->getDtRequest($nuclide);
-		 
-		$statement1 = $this->getDoctrine()->getManager()->getConnection()->prepare($rawsql);
-		$statement1->execute();
-		$resultdb = $statement1->fetchAll();
-		$table = $this->createDataTable()
-		->add('referenceDate', DateTimeColumn::class, ['format' => 'd-m-Y', 'label' => 'table.refdate', 'className' => 'bold'])
-		->add('limited', TextColumn::class, ['label' => 'table.limited', 'visible' => false])
-		->add('value', TextColumn::class, ['label' => 'table.value', 'raw' => true, 'render' => function($value, $context) {
-			if($context['limited']==0) {
-				return sprintf('%.1e', $context['value']);
-			} else {
-				return sprintf('&lt; %.1e', $context['value']);
-			}
-	
-		}])
-		->add('error', TextColumn::class, ['label' => 'table.error', 'render' => function($value, $context) {
-			if($context['error']=='') return '';
-			else return sprintf('%.1e', $context['error']);
-		}])
-		->add('unit', TextColumn::class, ['label' => 'table.unit'])
-		->add('station', TextColumn::class, ['label' => 'table.station'])
-		->createAdapter(ArrayAdapter::class, $resultdb)
-		->handleRequest($request);
-	
-		if ($table->isCallback()) {
-			return $table->getResponse();
-		}
-	
-		return $this->render('main_map.html.twig', array(
-				'legends' => $legends,
-				'siteTypes' => $siteTypes,
-				'automaticNetworks' => $automaticNetworks,
-				'zooms' => $zooms,
-				'datatable' => $table,
-		));
-	
-	}
-	
-	
-	public function getDtRequest($nuclide)
-	{
-		if($nuclide==NULL) $nuclide=21;
-		
-		// query builder
-		$rawsql = 'SELECT r.limited, r.value, r.error, u.code as unit, r.nuclide_id, m.id, m.referenceDate, st.code as station, st.id
-FROM result r
-left join measurement m on r.measurement_id=m.id
-left join sample s on m.sample_id=s.id
-left join station st on s.station_id=st.id
-left join result_unit u on m.result_unit_id=u.id
-INNER JOIN
-(SELECT max(m.referenceDate) as maxdate, st.code as st_code
-FROM measurement m
-left join sample s on m.sample_id=s.id
-left join station st on s.station_id=st.id
-WHERE st.code LIKE "HV%"
-group by st.code) AS groupe1
-  
-ON m.referenceDate=groupe1.maxdate
-and st.code=groupe1.st_code
-WHERE r.nuclide_id=' . $nuclide
-	. ' ORDER BY m.referenceDate DESC';
-	 
-	return $rawsql;
-	}
 	
 }
