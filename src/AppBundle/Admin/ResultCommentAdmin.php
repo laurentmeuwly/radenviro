c040f66c189645824513fc90dde012e04e3af542
Laurent Meuwly <laurent@lmeuwly.ch>
2019-07-30 09:21:33 +0200

    Modify namespace

---
+++ src/AppBundle/Admin/ResultCommentAdmin.php
@@ -0,0 +1,85 @@
+<?php 
+// src/AppBudle/Admin/ResultCommentAdmin.php
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Show\ShowMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use A2lix\TranslationFormBundle\Form\Type\TranslationsType;
+
+class ResultCommentAdmin extends AbstractAdmin
+{
+	// Fields to be shown on lists
+	protected function configureListFields(ListMapper $listMapper)
+	{
+		$listMapper
+		->addIdentifier('id')
+		//->add('name', null, array('label' => 'admin.network.name'))
+		//->add('networkCategory.name')
+		->add('dateFrom')
+		->add('dateTo')
+		->add('station')
+		->add('network')
+		->add('_active', 'boolean', array('label' => 'admin.label.active',
+				'editable' => true,
+				'header_style' => 'text-align: center',
+				'row_align' => 'center')
+				)
+		->add('_action', 'actions', array(
+				'actions' => array(
+						'edit' => array(),
+						'delete' => array(),
+				)
+			))
+		;
+	}
+	
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function configureFormFields(FormMapper $formMapper)
+	{
+		$formMapper
+		->with('General', array('class' => 'col-md-3', 'label' => 'admin.label.general'))
+			//->add('id')
+			->add('dateFrom')
+			->add('dateTo')
+			->add('station')
+			->add('network')
+			->add('active', null, array('label' => 'admin.label.active'))
+		->end()
+		->with('Informations', array('class' => 'col-md-6', 'label' => 'admin.label.informations'))
+			->add('translations', TranslationsType::class, array(
+					'label' => false,
+					'fields' => array(
+							'comment'=> array('label' => 'admin.result.comment')
+					)
+			))
+		->end()
+		->with('History', array('class' => 'col-md-3', 'label' => 'admin.label.history'))
+		->add('createdAt', 'sonata_type_datetime_picker',  array('label' => 'admin.label.created_at',
+				'attr' => array(
+						'readonly' => true
+				)
+		))
+		->add('updatedAt', 'sonata_type_datetime_picker', array('label' => 'admin.label.updated_at',
+				'attr' => array(
+						'readonly' => true
+				)
+		))
+		->end()
+		;
+	}
+	
+	protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+	{
+		$datagridMapper
+		->add('active', null, ['operator_type' => 'sonata_type_boolean', 'label' => 'admin.label.active'])
+		->add('station')
+		//->add('translations.name', null, ['label' => 'admin.network.name'])
+		;
+	}
+}
